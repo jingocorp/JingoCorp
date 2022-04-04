a4853921ea170abd1569b92b29b32d1772f62ffc
jingocorp <jingo.corp@gmail.com>
2022-04-05 00:19:15 +0530

    updated path resolve

--- Backend/app.js
+++ Backend/app.js
@@ -56,7 +56,9 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("Frontend/build"));
     const path = require("path");
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+        res.sendFile(
+            path.resolve(__dirname + "/Backend/Frontend/build/index.html")
+        );
     });
 }
 
